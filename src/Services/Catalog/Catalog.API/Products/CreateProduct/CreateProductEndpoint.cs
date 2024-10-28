a9b73f828edce7cfbaacb935b7b771b400656046
Jeronimo Fermin Deiros <jeronimo.deiros@globant.com>
2024-10-28 09:38:16 -0300

    Develop Vertical Slice Feature Folder with CQRS and MediatR

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,8 @@
+﻿// esta representa nuestra capa de presentacion
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public class CreateProductEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,8 @@
+﻿//representa la capa logica de nuestra aplicacion.
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public class CreateProductHandler
+    {
+    }
+}
