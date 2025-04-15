478e4af5bb25c42700885cd03e3a8ceffc430890
Jero <jeronimo.deiros@globant.com>
2025-04-15 20:57:24 -0300

    159. Seed and auto-migrate SQLite database using EntityFramework Core.

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -19,7 +19,7 @@ public class DiscountContext : DbContext
             new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150},
             new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100}
             );
-        base.OnModelCreating(modelBuilder);
+        
     }
 }
 
