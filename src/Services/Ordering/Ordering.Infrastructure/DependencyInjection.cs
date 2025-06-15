85ca3aed71142ef389e71a817c711acfa5fddc8c
jdeiros <jeronimodeiros@gmail.com>
2025-06-14 21:32:23 -0300

    187. Adding Dependency Injections Between Clean Architecture Layers.

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Register infrastructure services here
+        // Example: services.AddScoped<IOrderRepository, OrderRepository>();
+        // services.AddDbContext<OrderingContext>(options => 
+        //     options.UseSqlServer("YourConnectionStringHere"));
+
+        var connectionString = configuration.GetConnectionString("Database");
+
+        //// Add services to the container.
+        //services.AddDbContext<OrderingContext>(options =>
+        //     options.UseSqlServer(connectionString));
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        return services;
+    }
+}
