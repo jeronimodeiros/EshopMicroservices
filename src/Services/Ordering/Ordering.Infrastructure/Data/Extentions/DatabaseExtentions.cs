46c2f38d7c25214476b686cffac2b117d3778433
Jero <jeronimo.deiros@globant.com>
2025-09-30 17:53:33 -0300

    216. Seed SQL Server OrderDb using EF Core when Application Startup with Coding.

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extentions/DatabaseExtentions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extentions/DatabaseExtentions.cs
@@ -12,5 +12,24 @@ public static class DatabaseExtentions
 		var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
 		context.Database.MigrateAsync().GetAwaiter().GetResult(); // Ensure database is created and apply migrations
+
+		await SeedAsync(context);
+	}
+
+	private static async Task SeedAsync(ApplicationDbContext context)
+	{
+		await SeedCustomerAsync(context);
+		//await SeedProductAsync(context);
+		//await SeedOrderandItemsAsync(context);
+
+	}
+
+	private static async Task SeedCustomerAsync(ApplicationDbContext context)
+	{
+		if (!await context.Customers.AnyAsync())
+		{
+			await context.Customers.AddRangeAsync(InitialData.Customers);
+			await context.SaveChangesAsync();
+		}
 	}
 }
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extentions/InitialData.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Infrastructure.Data.Extentions;
+
+internal class InitialData
+{
+	public static IEnumerable<Customer> Customers => new List<Customer>
+	{
+		Customer.Create(CustomerId.Of(new Guid("d290f1ee-6c54-4b01-90e6-d701748f0851")), "John Doe", "john@gmail.com"),
+		Customer.Create(CustomerId.Of(new Guid("c56a4180-65aa-42ec-a945-5fd21dec0538")), "Jane Smith", "janes.smith@gmail.com")
+	};
+}
