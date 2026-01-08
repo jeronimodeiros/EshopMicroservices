d626a34462b9a7000de47eb4994c13279456ab0b
jdeiros <jeronimodeiros@gmail.com>
2026-01-08 09:50:01 -0300

    232 - Develop Ordering.Application Folder Structure for CQRS

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application;
 
@@ -8,9 +9,10 @@ public static class DependencyInjection
     {
         // Register application services here
         // Example: services.AddScoped<IOrderService, OrderService>();
-        //services.AddMediatR(cfg => {
-        //      cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-        // });
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
         return services;
     }
 }
