ebf9ff8ce0a2e716ff063a657be0791f3558953c
jdeiros <jeronimodeiros@gmail.com>
2026-01-09 12:05:11 -0300

    S16: Implement application layer CQRS - create order handler (work in progress)

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+	public CreateOrderCommandValidator()
+	{
+		
+		RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("CustomerId is required.");
+		RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("OrderName is required.");
+		RuleFor(x => x.Order.OrderItems)
+			.NotEmpty()
+			.WithMessage("OrderItems are required.")
+			.Must(oi => oi.Count > 0)
+			.WithMessage("At least one OrderItem is required.");
+	}
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+	public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+	{
+		//create Order entity from command object
+		//save to database
+		//return result with new order id
+
+		throw new NotImplementedException();
+	}
+}
