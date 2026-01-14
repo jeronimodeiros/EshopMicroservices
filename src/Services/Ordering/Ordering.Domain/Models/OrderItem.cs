bacc83bf41e8ac8dc45930e9723cf979fc08a963
jdeiros <jeronimodeiros@gmail.com>
2026-01-14 11:52:15 -0300

    207. Mapping DDD Objects to EF Entities - Develop Order Entity Configurations.

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -2,19 +2,18 @@
 
 public class OrderItem : Entity<OrderItemId>
 {
-    public Guid OrderId { get; private set; } = default!;
-    public Guid ProductId { get; private set; } = default!;
-    public decimal Price { get; private set; } = default!;
-    public int Quantity { get; private set; } = default!;
-
-
     internal OrderItem(OrderId orderId, ProductId productId, decimal price, int quantity)
     {
         Id = OrderItemId.Of(Guid.NewGuid());
-        OrderId = orderId.Value;
-        ProductId = productId.Value;
+        OrderId = orderId;
+        ProductId = productId;
         Price = price;
         Quantity = quantity;
     }
+
+    public OrderId OrderId { get; private set; } = default!;
+	public ProductId ProductId { get; private set; } = default!;
+	public int Quantity { get; private set; } = default!;
+	public decimal Price { get; private set; } = default!;	
 
 }
