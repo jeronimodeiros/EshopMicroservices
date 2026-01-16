cc38d2569a72e2fb3d79a2a610f2d5cb6dd7f292
jdeiros <jeronimodeiros@gmail.com>
2026-01-16 15:20:31 -0300

    211. Create EF Core Migrations for ApplicationDbContext in Ordering.Infrastructure-2

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -2,14 +2,17 @@
 public class Order : Aggregate<OrderId>
 {
     private readonly List<OrderItem> _orderItems = new();
+    private decimal _totalPrice; // backing field persistible
+
     public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+    public decimal TotalPrice { get; private set; }
+
     public CustomerId CustomerId { get; private set; } = default!;
     public OrderName OrderName { get; private set; } = default!;
     public Address ShippingAddress { get; private set; } = default!;
     public Address BillingAddress { get; private set; } = default!;
     public Payment Payment { get; private set; } = default!;
     public OrderStatus Status { get; private set; } = OrderStatus.Pending;
-    public decimal TotalPrice => _orderItems.Sum(item => item.Price * item.Quantity);
     public static Order Create(
         OrderId id,
         CustomerId customerId,
@@ -44,13 +47,15 @@ public class Order : Aggregate<OrderId>
         AddDomainEvent(new OrderUpdatedEvent(this));
     }
 
+    // cuando agregues o elimines items actualiza _totalPrice
     public void Add(ProductId productId, decimal price, int quantity)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity, nameof(quantity));
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price, nameof(price));
         var orderItem = new OrderItem(Id, productId, price, quantity);
         _orderItems.Add(orderItem);
-        //AddDomainEvent(new OrderItemAddedEvent(this, orderItem));
+
+        _totalPrice = _orderItems.Sum(i => i.Price * i.Quantity);
     }
 
     public void Remove(ProductId productId)
@@ -59,8 +64,7 @@ public class Order : Aggregate<OrderId>
         if (orderItem is not null)
         {
             _orderItems.Remove(orderItem);
+            _totalPrice = _orderItems.Sum(i => i.Price * i.Quantity);
         }
-        
-        //AddDomainEvent(new OrderItemRemovedEvent(this, orderItem));
     }
 }
--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Ordering.Domain.Enums;
 using Ordering.Domain.Models;
@@ -26,69 +25,99 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
 			.WithOne()
 			.HasForeignKey(oi => oi.OrderId);
 
-		builder.ComplexProperty(
-			o => o.OrderName, nameBuilder =>
-			{
-				nameBuilder.Property(n => n.Value)
-					.HasColumnName(nameof(Order.OrderName))
-					.HasMaxLength(100)
-					.IsRequired();
-			});
+		// OrderName as owned/value object
+		builder.OwnsOne(o => o.OrderName, nameBuilder =>
+		{
+			nameBuilder.Property(n => n.Value)
+				.HasColumnName(nameof(Order.OrderName))
+				.HasMaxLength(100)
+				.IsRequired();
+		});
 
-		builder.ComplexProperty(
-			o => o.ShippingAddress, addressBuilder =>
-			{
-				addressBuilder.Property(a => a.FirstName)
-					.HasMaxLength(50)
-					.IsRequired();
-				
-				addressBuilder.Property(a => a.LastName)
-					.HasMaxLength(50)
-					.IsRequired();
+		// ShippingAddress as owned/value object
+		builder.OwnsOne(o => o.ShippingAddress, addressBuilder =>
+		{
+			addressBuilder.Property(a => a.FirstName)
+				.HasMaxLength(50)
+				.IsRequired();
+			
+			addressBuilder.Property(a => a.LastName)
+				.HasMaxLength(50)
+				.IsRequired();
 
-				addressBuilder.Property(a => a.EmailAddress)
-					.HasMaxLength(50);
+			addressBuilder.Property(a => a.EmailAddress)
+				.HasMaxLength(50);
 
-				addressBuilder.Property(a => a.AddressLine)
-					.HasMaxLength(180)
-					.IsRequired();
-				addressBuilder.Property(a => a.Country)
-					.HasMaxLength(50);
+			addressBuilder.Property(a => a.AddressLine)
+				.HasMaxLength(180)
+				.IsRequired();
+			addressBuilder.Property(a => a.Country)
+				.HasMaxLength(50);
 
-				addressBuilder.Property(a => a.State)
-					.HasMaxLength(50);
+			addressBuilder.Property(a => a.State)
+				.HasMaxLength(50);
 
-				addressBuilder.Property(a => a.ZipCode)
-					.HasMaxLength(5)
-					.IsRequired();
-			});
+			addressBuilder.Property(a => a.ZipCode)
+				.HasMaxLength(5)
+				.IsRequired();
+		});
 
-		builder.ComplexProperty(
-			o => o.Payment, paymentBuilder =>
-			{
-				paymentBuilder.Property(p => p.CardName)
-					.HasMaxLength(50);
+		// BillingAddress as owned/value object
+		builder.OwnsOne(o => o.BillingAddress, addressBuilder =>
+		{
+			addressBuilder.Property(a => a.FirstName)
+				.HasMaxLength(50)
+				.IsRequired();
+			
+			addressBuilder.Property(a => a.LastName)
+				.HasMaxLength(50)
+				.IsRequired();
 
-				paymentBuilder.Property(p => p.CardNumber)
-					.HasMaxLength(24)
-					.IsRequired();
+			addressBuilder.Property(a => a.EmailAddress)
+				.HasMaxLength(50);
 
-				paymentBuilder.Property(p => p.ExpirationDate)
-					.HasMaxLength(10);
+			addressBuilder.Property(a => a.AddressLine)
+				.HasMaxLength(180)
+				.IsRequired();
+			addressBuilder.Property(a => a.Country)
+				.HasMaxLength(50);
 
-				paymentBuilder.Property(p => p.Cvv)
-					.HasMaxLength(3);
+			addressBuilder.Property(a => a.State)
+				.HasMaxLength(50);
 
-				paymentBuilder.Property(p => p.PaymentMethod);
+			addressBuilder.Property(a => a.ZipCode)
+				.HasMaxLength(5)
+				.IsRequired();
+		});
 
-			});
+		// Payment as owned/value object
+		builder.OwnsOne(o => o.Payment, paymentBuilder =>
+		{
+			paymentBuilder.Property(p => p.CardName)
+				.HasMaxLength(50);
+
+			paymentBuilder.Property(p => p.CardNumber)
+				.HasMaxLength(24)
+				.IsRequired();
+
+			paymentBuilder.Property(p => p.ExpirationDate)
+				.HasMaxLength(10);
+
+			paymentBuilder.Property(p => p.Cvv)
+				.HasMaxLength(3);
+
+			paymentBuilder.Property(p => p.PaymentMethod);
+		});
 
 		builder.Property(o => o.Status)
 			.HasDefaultValue(OrderStatus.Draft)
 			.HasConversion(
 				s => s.ToString(),
-				dbStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus), dbStatus));
+				dbStatus => Enum.Parse<OrderStatus>(dbStatus));
 
-		builder.Property(o => o.TotalPrice);
+		// Mapear la propiedad TotalPrice al campo privado _totalPrice
+		builder.Property(o => o.TotalPrice)
+			.HasField("_totalPrice")
+			.UsePropertyAccessMode(PropertyAccessMode.Field);
 	}
 }
