f17ed52b30cf33f709028381d0b064ac36c7297a
jdeiros <jeronimodeiros@gmail.com>
2026-01-16 12:16:28 -0300

    209. Register EF Core DbContext in Asp.Net DependencyInjection Tool for SQL Server.

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Ordering.Application.Data;
+
 
 namespace Ordering.Infrastructure;
 
@@ -9,20 +8,14 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        // Register infrastructure services here
-        // Example: services.AddScoped<IOrderRepository, OrderRepository>();
-        // services.AddDbContext<OrderingContext>(options => 
-        //     options.UseSqlServer("YourConnectionStringHere"));
-
         var connectionString = configuration.GetConnectionString("Database");
 
-        //// Add services to the container.
-  //      services.AddDbContext<IApplicationDbContext>((sp, options) =>
-  //      {
-  //          options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
-		//	options.UseSqlServer(connectionString);
-		//});
+        // Add services to the container.
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
         return services;
     }
 }
