01a7c2866fdfffce3092224c73caa7038f723002
jdeiros <jeronimodeiros@gmail.com>
2026-01-20 12:54:50 -0300

    223. Develop DispatchDomainEventsInterceptor to Trigger Domain Event Handlers.

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -2,17 +2,15 @@
 using System.Reflection;
 
 namespace Ordering.Application;
-
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-    {
-        // Register application services here
-        // Example: services.AddScoped<IOrderService, OrderService>();
-        services.AddMediatR(cfg =>
-        {
-            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-        });
-        return services;
-    }
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddMediatR(cfg =>
+		{
+			cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+		});
+
+		return services;
+	}
 }
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,24 +1,27 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-
 namespace Ordering.Infrastructure;
-
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-    {
-        var connectionString = configuration.GetConnectionString("Database");
+	public static IServiceCollection AddInfrastructureServices
+		(this IServiceCollection services, IConfiguration configuration)
+	{
+		var connectionString = configuration.GetConnectionString("Database");
 
-        // Add services to the container.
-        services.AddDbContext<ApplicationDbContext>(options =>
-        {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+		// Add services to the container.
+		services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+		services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+		services.AddDbContext<ApplicationDbContext>((sp, options) =>
+		{
+			options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
 			options.UseSqlServer(connectionString);
 		});
 
-        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+		//services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
-        return services;
-    }
+		return services;
+	}
 }
