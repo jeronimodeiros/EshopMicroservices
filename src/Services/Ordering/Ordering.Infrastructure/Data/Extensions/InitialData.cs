3be1f07b0ef4a14bb7740218f3826641e35e06f0
jdeiros <jeronimodeiros@gmail.com>
2026-01-20 14:38:42 -0300

    create distinct Address instances for shipping and billing to avoid EF owned tracking error

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -21,8 +21,12 @@ internal class InitialData
 	{
 		get
 		{
-			var address1 = Address.Of("mehmet", "ozkaya", "mehmet@gmail.com", "Bahcelievler No:4", "Turkey", "Istanbul", "38050");
-			var address2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
+			// Create separate instances for shipping and billing even if values are equal
+			var shipping1 = Address.Of("mehmet", "ozkaya", "mehmet@gmail.com", "Bahcelievler No:4", "Turkey", "Istanbul", "38050");
+			var billing1 = Address.Of("mehmet", "ozkaya", "mehmet@gmail.com", "Bahcelievler No:4", "Turkey", "Istanbul", "38050");
+
+			var shipping2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
+			var billing2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
 
 			var payment1 = Payment.Of("mehmet", "5555555555554444", "12/28", "355", 1);
 			var payment2 = Payment.Of("john", "8885555555554444", "06/30", "222", 2);
@@ -31,8 +35,8 @@ internal class InitialData
 							OrderId.Of(Guid.NewGuid()),
 							CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")),
 							OrderName.Of("ORD_1"),
-							shippingAddress: address1,
-							billingAddress: address1,
+							shippingAddress: shipping1,
+							billingAddress: billing1,
 							payment1);
 			order1.Add(ProductId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), 2, 500);
 			order1.Add(ProductId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), 1, 400);
@@ -41,8 +45,8 @@ internal class InitialData
 							OrderId.Of(Guid.NewGuid()),
 							CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")),
 							OrderName.Of("ORD_2"),
-							shippingAddress: address2,
-							billingAddress: address2,
+							shippingAddress: shipping2,
+							billingAddress: billing2,
 							payment2);
 			order2.Add(ProductId.Of(new Guid("4f136e9f-ff8c-4c1f-9a33-d12f689bdab8")), 1, 650);
 			order2.Add(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), 2, 450);
